8041ae4130d39978c5c7f4910b1c4d37f5c9ad10
Victor-Araneda <110746074+Victor-Araneda@users.noreply.github.com>
2022-08-09 20:15:22 -0400

    Consumo de API

---
+++ prueba.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+
+def obtener_token(usuario, clave):
+    url = "https://10.10.20.14/api/aaaLogin.json"
+    body = {
+        "aaaUser": {
+            "attributes": {
+                "name": usuario,
+                "pwd": clave
+            }
+        }
+    }
+    cabecera = {
+        "Content-Type": "application/json"
+    }
+    requests.packages.urllib3.disable_warnings()
+    respuesta = requests.post(url, headers=cabecera, data=json.dumps(body), verify=False)
+    token = respuesta.json()['imdata'][0]['aaaLogin']['attributes']['token']
+    return token
+
+
+token = obtener_token("admin", "C1sco12345")
+print(token)
---
+++ prueba4.py
@@ -0,0 +1,38 @@
+import requests
+import json
+
+sandbox = "https://10.10.20.14"
+def obtener_token(usuario, clave):
+    url = sandbox + "/api/aaaLogin.json"
+    body = {
+        "aaaUser": {
+            "attributes": {
+                "name": usuario,
+                "pwd": clave
+            }
+        }
+    }
+    cabecera = {
+        "Content-Type": "application/json"
+    }
+    requests.packages.urllib3.disable_warnings()
+    respuesta = requests.post(url, headers=cabecera, data=json.dumps(body), verify=False)
+    token = respuesta.json()['imdata'][0]['aaaLogin']['attributes']['token']
+    return token
+
+token = obtener_token("admin", "C1sco12345")
+
+
+def info_nodo():
+    url = sandbox + "/api/node/class/fvCEp.xml"
+    cabecera = {
+        "Content-Type": "application/json"
+    }
+    variables = {
+        "APIC-Cookie": token
+    }
+    respuesta = requests.get(url, headers=cabecera, cookies=variables, verify=False)
+    print(respuesta.json())
+
+
+info_nodo()
